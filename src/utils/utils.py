8d18799ff64f1ed5fc7dda48e172e1f1edb02d00
Samuel Garcin <samgrcn@proton.me>
2024-11-03 10:18:21 +0100

    2D Version

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,12 +1,11 @@
-
 import torch
 
 def dice_coefficient(preds, targets, smooth=1e-5):
     preds = torch.sigmoid(preds)
     preds = (preds > 0.5).float()
 
-    intersection = (preds * targets).sum()
-    union = preds.sum() + targets.sum()
+    intersection = (preds * targets).sum(dim=(1, 2, 3))
+    union = preds.sum(dim=(1, 2, 3)) + targets.sum(dim=(1, 2, 3))
 
     dice = (2.0 * intersection + smooth) / (union + smooth)
-    return dice
+    return dice.mean()
